f8479d93ddfeb6606db62f1c931f86008be899af
rere2133 <reckie2133@gmail.com>
2023-12-15 15:12:14 +0800

    vector arrow

--- src/views/Vector.vue
+++ src/views/Vector.vue
@@ -28,6 +28,7 @@ const drawVector = (v, trans) => {
   ctx.save();
   ctx.rotate(v.angle());
   ctx.fillText(v, v.length() / 2 - 16, 12);
+  ctx.lineWidth = 5;
   ctx.lineTo(v.length(), 0);
   ctx.lineTo(v.length() - 5, -4);
   ctx.lineTo(v.length() - 5, 4);
@@ -40,35 +41,47 @@ const drawVector = (v, trans) => {
   }
 };
 const draw = () => {
-  // Method Pactice
-  let a = new Vector(4, 0);
-  let b = new Vector(0, 3);
-  let c = a.add(b);
-  let d = a.sub(b);
-  console.log(`${a}+${b}=${c}`);
-  console.log(`${a}-${b}=${d}`);
-  let b2 = b.mul(2);
-  console.log(`${b}*2=${b2}`);
-  console.log(`${c} length: ${c.length()}`);
-  d.set(0, 6);
-  console.log(b2.equal(d));
-  let newa = a.clone();
-  newa.move(3, 4);
-  console.log(newa, a);
+  ctx.clearRect(0, 0, ww.value, wh.value);
+  //// Method Pactice
+  // let a = new Vector(4, 0);
+  // let b = new Vector(0, 3);
+  // let c = a.add(b);
+  // let d = a.sub(b);
+  // console.log(`${a}+${b}=${c}`);
+  // console.log(`${a}-${b}=${d}`);
+  // let b2 = b.mul(2);
+  // console.log(`${b}*2=${b2}`);
+  // console.log(`${c} length: ${c.length()}`);
+  // d.set(0, 6);
+  // console.log(b2.equal(d));
+  // let newa = a.clone();
+  // newa.move(3, 4);
+  // console.log(newa, a);
 
-  // Draw Vector
-  let v1 = new Vector(250, 0);
-  let v2 = new Vector(0, 200);
-  let v3 = v1.add(v2).mul(-1);
-  ctx.translate(ww.value / 2, wh.value / 2);
-  drawVector(v1, true);
-  drawVector(v2, true);
-  drawVector(v3, true);
+  // // Draw Vector
+  // let v1 = new Vector(250, 250);
+  // let v2 = new Vector(0, 200);
+  // let v3 = v1.add(v2).mul(-1);
+  // ctx.translate(ww.value / 2, wh.value / 2);
+  // drawVector(v1, true);
+  // drawVector(v2, true);
+  // drawVector(v3, true);
+
+  // Vector Arrow
+  let c = new Vector(ww.value / 2, wh.value / 2);
+  if (mouse.value?.x) {
+    ctx.save();
+    ctx.translate(ww.value / 2, wh.value / 2);
+    let md = mouse.value.sub(c);
+    drawVector(md.mul(1 / md.length()).mul(150), false);
+    ctx.restore();
+  }
 };
 onMounted(() => {
   init();
-  // setInterval(draw, 30);
-  draw();
+  setInterval(draw, 30);
+  // setTimeout(draw, 0);
+  // draw();
 });
 </script>
 
