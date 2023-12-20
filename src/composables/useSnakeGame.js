3ab3bdf4911b37eff0d50528c81cfb423a95d2b4
rere2133 <reckie2133@gmail.com>
2023-12-20 18:13:21 +0800

    add keydown

--- src/composables/useSnakeGame.js
+++ src/composables/useSnakeGame.js
@@ -1,89 +1,175 @@
 import Vector from './useVector'
 import { ref } from 'vue'
+import * as Tone from 'tone'
 
-export function useSnakeGame(){
-
+export function useSnakeGame() {
+  // Snake
   const snake = ref({
     body: [],
     maxLength: 5,
     head: new Vector(),
-    speed: new Vector(1,0),
+    speed: new Vector(1, 0),
     direction: 'Right'
   })
-  const snakeUpdate = ()=>{
+  const snakeUpdate = () => {
     let newHead = snake.value.head.add(snake.value.speed)
     snake.value.body.push(snake.value.head)
     snake.value.head = newHead
-    while(snake.value.body.length > snake.value.maxLength){
+    while (snake.value.body.length > snake.value.maxLength) {
       snake.value.body.shift()
     }
   }
-  const setDirection = (dir)=>{
+  const setDirection = (dir) => {
     // console.log(dir);
     let target
-    switch(dir){
+    switch (dir) {
       case 'Up':
-        target = new Vector(0,-1)
+        target = new Vector(0, -1)
         break;
       case 'Down':
-        target = new Vector(0,1)
+        target = new Vector(0, 1)
         break;
       case 'Left':
-        target = new Vector(-1,0)
+        target = new Vector(-1, 0)
         break;
       case 'Right':
-        target = new Vector(1,0)
+        target = new Vector(1, 0)
         break;
     }
-    if(target.equal(snake.value.speed.mul(-1))==false){
+    if (target.equal(snake.value.speed.mul(-1)) == false) {
       snake.value.speed = target
     }
   }
+  const checkBoundary = () => {
+    let xInRange = 0 <= snake.value.head.x && snake.value.head.x < game.value.gameWidth + 1
+    let yInRange = 0 <= snake.value.head.y && snake.value.head.y < game.value.gameWidth + 1
+    console.log();
+    return xInRange && yInRange
+  }
 
+  // Game
+  const score = ref(0)
   const game = ref({
     blockWidth: 12, //小格子寬度
-    blockGap:2, //格子間寬度
+    blockGap: 2, //格子間寬度
     gameWidth: 40, // 遊戲邊長有幾個格子
     speed: 30,
-    // snake: snake.value,
-    food: []
+    foods: [],
+    start: false
   })
-  const getPosition = (x,y)=>{
+  // 遊戲開始
+  const startGame = () => {
+    game.value.start = true
+  }
+  // 遊戲結束
+  const endGame = () => {
+    game.value.start = false
+    score.value = (snake.value.body.length - 5) * 10
+    snake.value = {
+      body: [],
+      maxLength: 5,
+      head: new Vector(),
+      speed: new Vector(1, 0),
+      direction: 'Right'
+    }
+  }
+  // 尋找實際向量位置
+  const getPosition = (x, y) => {
     return new Vector
-    (x*game.value.blockWidth + (x-1)*game.value.blockGap,
-    y*game.value.blockWidth + (y-1)*game.value.blockGap)
+      (x * game.value.blockWidth + (x - 1) * game.value.blockGap,
+        y * game.value.blockWidth + (y - 1) * game.value.blockGap)
   }
-
-  const drawBlock = (ctx,v,color)=>{
+  // 畫格子
+  const drawBlock = (ctx, v, color) => {
     ctx.fillStyle = color
-    let pos = getPosition(v.x,v.y)
-    ctx.fillRect(pos.x,pos.y,game.value.blockWidth,game.value.blockWidth)
+    let pos = getPosition(v.x, v.y)
+    ctx.fillRect(pos.x, pos.y, game.value.blockWidth, game.value.blockWidth)
   }
-
-  const render = (ctx,canvasW,canvasH)=>{
-    ctx.fillStyle = "rgba(0,0,0,0.3)"
-    ctx.fillRect(0,0,canvasW,canvasH)
-    for(let x=0;x<game.value.gameWidth;x++){
-      for(let y=0;y<game.value.gameWidth;y++){
-        drawBlock(ctx,new Vector(x,y),'rgba(255,255,255,0.03)')
+  // 食物產生時的效果
+  const drawEffect = (ctx, x, y) => {
+    let r = 2
+    let pos = getPosition(x, y)
+    const effect = () => {
+      r++
+      ctx.strokeStyle = `rgba(255,0,0,${(100 - r) / 100})`
+      ctx.beginPath()
+      ctx.arc(pos.x + game.value.blockWidth / 2, pos.y + game.value.blockWidth / 2, r, 0, Math.PI * 2)
+      ctx.stroke()
+      if (r < 100) {
+        requestAnimationFrame(effect)
       }
     }
-    snake.value.body.forEach(sp=>{
-      drawBlock(ctx,sp,'#fff')
-    })
-    requestAnimationFrame(()=>render(ctx,canvasW,canvasH))
+    requestAnimationFrame(effect)
   }
 
-  const update = ()=>{
-    snakeUpdate()
-    setTimeout(()=>{
-      update()
-    },150)
+  // 產生食物
+  const generateFood = (ctx) => {
+    let x = parseInt(Math.random() * game.value.gameWidth)
+    let y = parseInt(Math.random() * game.value.gameWidth)
+    game.value.foods.push(new Vector(x, y))
+    drawEffect(ctx, x, y)
   }
-  return{
+  // 音效
+  const playSound = (note, volume, when) => {
+    setTimeout(() => {
+      let synth = new Tone.Synth().toDestination()
+      synth.Volume = volume || -12
+      synth.triggerAttackRelease(note, '8n')
+    }, when || 0)
+  }
+
+  // 畫面更新
+  const render = (ctx, canvasW, canvasH) => {
+    ctx.fillStyle = "rgba(0,0,0,0.3)"
+    ctx.fillRect(0, 0, canvasW, canvasH)
+    for (let x = 0; x < game.value.gameWidth; x++) {
+      for (let y = 0; y < game.value.gameWidth; y++) {
+        drawBlock(ctx, new Vector(x, y), 'rgba(255,255,255,0.03)')
+      }
+    }
+    snake.value.body.forEach(sp => {
+      drawBlock(ctx, sp, '#fff')
+    })
+    game.value.foods.forEach(p => {
+      drawBlock(ctx, p, 'red')
+    })
+    requestAnimationFrame(() => render(ctx, canvasW, canvasH))
+  }
+
+  const update = (ctx) => {
+    if (game.value.start) {
+      playSound('A2', -50)
+      snakeUpdate()
+      // 蛇吃到食物
+      game.value.foods.forEach((food, i) => {
+        if (snake.value.head.equal(food)) {
+          snake.value.maxLength++
+          game.value.foods.splice(i, 1)
+          generateFood(ctx)
+        }
+      })
+      // 蛇撞到身體
+      snake.value.body.forEach(bp => {
+        if (snake.value.head.equal(bp)) [
+          endGame()
+        ]
+      })
+      // 蛇碰到邊界
+      if (!checkBoundary()) endGame()
+    }
+
+
+    setTimeout(() => {
+      update(ctx)
+    }, 150)
+  }
+  return {
     setDirection,
+    score,
     game,
+    startGame,
     render,
-    update
+    update,
+    generateFood
   }
 }
--- src/views/Snake.vue
+++ src/views/Snake.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-    <canvas class="myCanvas" ref="myCanvas"></canvas>
+  <div class="relative">
+    <div
+      v-if="!game.start"
+      class="absolute flex justify-center items-center flex-col w-full h-[600px]"
+    >
+      <h2 class="text-5xl text-white mb-4">Score: {{ score }}</h2>
+      <div
+        @click="startGame"
+        class="bg-black text-white border-2 py-1 px-3 border-white-100 max-w-[120px] text-sm rounded-full cursor-pointer hover:bg-slate-50 hover:text-slate-900"
+      >
+        Start Game
+      </div>
+    </div>
+    <canvas class="myCanvas" ref="myCanvas"> </canvas>
     <!-- For Test -->
-    <input @keydown.down="setDirection('Down')" />
+    <div class="mx-auto mt-5">
+      <div class="flex gap-2 mb-2 justify-center">
+        <div class="btn opacity-0"></div>
+        <div @click="setDirection('Up')" class="btn">↑</div>
+        <div class="btn opacity-0"></div>
+      </div>
+      <div class="flex gap-2 justify-center">
+        <div @click="setDirection('Left')" class="btn">←</div>
+        <div @click="setDirection('Down')" class="btn">↓</div>
+        <div @click="setDirection('Right')" class="btn">→</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import Vector from "@/composables/useVector";
 import { useSnakeGame } from "@/composables/useSnakeGame";
 const myCanvas = ref(null);
 let ctx = ref();
 const snakeGame = useSnakeGame();
-const { game, setDirection } = snakeGame;
+const { score, game, setDirection, startGame } = snakeGame;
 
 const init = () => {
   myCanvas.value.width =
@@ -23,7 +46,8 @@ const init = () => {
   ctx = myCanvas.value.getContext("2d");
 
   snakeGame.render(ctx, myCanvas.value.width, myCanvas.value.height);
-  snakeGame.update();
+  snakeGame.update(ctx);
+  snakeGame.generateFood(ctx);
   // myCanvas.value.addEventListener("mousemove", function (event) {
   //   mouse.value.x = event.offsetX;
   //   mouse.value.y = event.offsetY;
@@ -33,11 +57,27 @@ const draw = () => {
   // time.value += 1;
   // ctx.clearRect(0, 0, 400, 400);
 };
+const handleKeydown = (e) => {
+  if (e.key.indexOf("Arrow") != -1) {
+    setDirection(e.key.replace("Arrow", ""));
+  }
+  if (e.key == "Enter") {
+    startGame();
+  }
+};
 onMounted(() => {
   init();
   // setInterval(draw, 30);
   draw();
+  window.addEventListener("keydown", handleKeydown);
+});
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeydown);
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  @apply text-2xl w-16 h-16 bg-gray-300 rounded-lg flex items-center justify-center shadow-xl active:bg-gray-400 active:shadow-sm cursor-pointer;
+}
+</style>
